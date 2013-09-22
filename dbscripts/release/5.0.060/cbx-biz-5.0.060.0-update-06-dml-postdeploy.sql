19660e14a12ac3b4121e77fdc58710abe49ed61a
vincent.tan <vincent.tan@coresolutions.com>
2013-09-22 11:30:26 +0800

    CNT-10785

--- dbscripts/release/5.0.060/cbx-biz-5.0.060.0-update-06-dml-postdeploy.sql
+++ dbscripts/release/5.0.060/cbx-biz-5.0.060.0-update-06-dml-postdeploy.sql
@@ -6,6 +6,11 @@ UPDATE CNT_RFQ SET requested_user_name = requested_by;
 UPDATE CNT_RFQ rfq SET rfq.requested_by = (select login_id || '@' || domain_id from cnt_user where user_name = rfq.requested_user_name and rownum = 1);
 ---CNT-10709 END-----------
 
-
+---CNT-10785 BEGIN-----------
+UPDATE cnt_qc qc SET spec_id = (SELECT spec_id
+                    FROM cnt_item item
+                   WHERE qc.item_id = item.ID)
+ WHERE spec_id IS NULL;
+---CNT-10785 END-----------
 
 COMMIT;   
