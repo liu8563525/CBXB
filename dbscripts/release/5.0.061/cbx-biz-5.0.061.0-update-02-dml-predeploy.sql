86e232a4ed336527a5deda0d423e357547520098
vincent.tan <vincent.tan@coresolutions.com>
2013-10-24 09:59:43 +0800

    CNT-11125

--- dbscripts/release/5.0.061/cbx-biz-5.0.061.0-update-02-dml-predeploy.sql
+++ dbscripts/release/5.0.061/cbx-biz-5.0.061.0-update-02-dml-predeploy.sql
@@ -1,9 +1,9 @@
 WHENEVER SQLERROR EXIT sql.sqlcode ROLLBACK
 SET DEFINE OFF
 
---CNT-10658 BEGIN--------
+--CNT-11125 BEGIN--------
 UPDATE cnt_shipment_advice SET destination_forwarder='';
---CNT-10658 END--------
+--CNT-11125 END--------
 
 commit;
 
