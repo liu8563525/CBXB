125047b2de363ab86db9bcd2ff4efe4d59402bae
kevin.liu <kevin.liu@coresolutions.com>
2013-10-24 10:04:56 +0800

    CNT-11255

--- dbscripts/release/5.0.061/cbx-biz-5.0.061.0-update-02-dml-predeploy.sql
+++ dbscripts/release/5.0.061/cbx-biz-5.0.061.0-update-02-dml-predeploy.sql
@@ -5,6 +5,12 @@ SET DEFINE OFF
 UPDATE cnt_shipment_advice SET destination_forwarder='';
 --CNT-11125 END--------
 
+--CNT-11255 BEGIN--------
+UPDATE cnt_codelist_book
+   SET effective_to = ''
+ WHERE is_latest = 1 AND effective_to < SYSDATE;
+--CNT-11255 END--------
+
 commit;
 
 
