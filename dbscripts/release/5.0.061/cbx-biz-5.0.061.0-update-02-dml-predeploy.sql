b9942195ce8f8f7cd870ef6d3025331b8f51575d
vincent.tan <vincent.tan@coresolutions.com>
2013-11-01 13:44:33 +0800

    CNT-11385

--- dbscripts/release/5.0.061/cbx-biz-5.0.061.0-update-02-dml-predeploy.sql
+++ dbscripts/release/5.0.061/cbx-biz-5.0.061.0-update-02-dml-predeploy.sql
@@ -11,6 +11,18 @@ UPDATE cnt_codelist_book
  WHERE is_latest = 1 AND effective_to < SYSDATE;
 --CNT-11255 END--------
 
+--CNT-11385
+update CNT_DOMAIN_ATTRIBUTE set value='VPO Send Notification Profile' where key='notification.vpo.send.template';
+
+
+
+INSERT INTO CNT_DOMAIN_ATTRIBUTE (REVISION,ENTITY_VERSION,ID,DOMAIN_ID,KEY,VALUE,CATEGORY,TYPE,DESCRIPTION,HUB_DOMAIN_ID,IS_FOR_REFERENCE) 
+SELECT 0,1,Sys_Guid(),'/','export.vpo.ext','.xlsx',
+'Export Setting',0,'Export VPO file extension','/', 0 FROM DUAL 
+WHERE NOT EXISTS (SELECT 1 FROM CNT_DOMAIN_ATTRIBUTE WHERE DOMAIN_ID = '/' AND KEY = 'export.vpo.ext' );
+
+--end of CNT-11385
+
 commit;
 
 
