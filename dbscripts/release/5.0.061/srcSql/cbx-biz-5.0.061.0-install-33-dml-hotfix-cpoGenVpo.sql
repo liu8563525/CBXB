9ab47b47bd884a0bdefc7a5365f0d0fb0539d2c4
Gavin.Lin <gavin.lin@coresolutions.com>
2013-11-20 14:57:50 +0800

    Add hotfix

--- dbscripts/release/5.0.061/srcSql/cbx-biz-5.0.061.0-install-33-dml-hotfix-cpoGenVpo.sql
+++ dbscripts/release/5.0.061/srcSql/cbx-biz-5.0.061.0-install-33-dml-hotfix-cpoGenVpo.sql
@@ -151,7 +151,6 @@ destinationEntity=vpoShipDtl,vpoCharge,vpoShipDc,VpoDcDtl,
 destinationLookupKey=itemId
 destinationValueTo=vpoItemId
 destinationValueFrom=id
-#configuration to reset color size in vpo
 csParentCollectionId=vpoItem/vpoShipDtl
 colorCollectionId=vpoItemColor/vpoShipDtlColor
 sizeCollectionId=vpoItemSize/vpoShipDtlSize
@@ -171,3 +170,31 @@ csFieldValueFormColor=cpoItemColorId/
 csFieldValueFormSize=cpoItemSizeId/
 ' FROM DUAL;
 INSERT INTO CNT_CUSTOMIZED_PROCESSOR(ID, DATA_MAPPING_RULE_ID, DOMAIN_ID, REVISION, ENTITY_VERSION, PROCESSOR_TYPE, TEMPLATE_ID, PROCESSOR_IMPL) SELECT '1bc0938fb9f249b5aaf5fedfeb6ce074', (SELECT ID FROM CNT_DATA_MAPPING_RULE WHERE SOURCE_ENTITY_NAME = 'Cpo' AND SOURCE_ENTITY_VERSION = '1' AND DMR_NAME = 'defaultCpoGenVpo' AND DOMAIN_ID = '/'), '/', '0', '1', 'PostProcessor', '377b4564d97949df897959386d8b2210', 'com.core.cbx.inheritance.customizedprocessor.CpoGenVpoProcessor' FROM DUAL;
+
+DECLARE
+  TYPE default_tables IS TABLE OF VARCHAR2 (1000) INDEX BY VARCHAR2 (100); 
+  default_table default_tables ;
+  default_table_key VARCHAR2 (100);
+  update_sql VARCHAR2(1000);
+BEGIN 
+  -- Define column name and its default values here
+  default_table('IS_FOR_REFERENCE') := '0';
+  default_table('HUB_DOMAIN_ID') := 'DOMAIN_ID';
+  default_table('IS_LATEST') := 1;
+
+  default_table_key := default_table.FIRST;
+  while(default_table_key IS NOT NULL)
+    LOOP
+        FOR t IN (SELECT TABLE_NAME FROM USER_TABLES UT WHERE EXISTS (SELECT 1 FROM USER_TAB_COLUMNS WHERE TABLE_NAME = UT.TABLE_NAME AND COLUMN_NAME = default_table_key))
+          LOOP
+            update_sql := 'UPDATE ' || t.TABLE_NAME || ' SET ' || default_table_key || ' = ' || default_table(default_table_key) || ' WHERE ' || default_table_key || ' IS NULL';
+            EXECUTE IMMEDIATE update_sql;
+          END LOOP;
+        default_table_key := default_table.NEXT(default_table_key);
+    END LOOP;
+END;
+/
+
+DELETE FROM CNT_SERIALIZED_ENTITY WHERE TARGET_ENTITY IN ('DataMappingRule', 'DataInheritanceProfile');
+
+COMMIT;
